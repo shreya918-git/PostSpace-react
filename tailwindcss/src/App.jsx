868f00b32e2cc443311417ec7331ceee8f1bcbf0
shreya918-git <shreyasharma918nds@gmail.com>
2024-11-04 10:53:07 +0530

    tailwindinsertions

---
+++ tailwindcss/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Embed from './Embed1.jsx'
+function App() {
+  var obj={
+    username:"shreya",
+    age:20
+  }
+  return (
+    <div className="d-flex flex-column align-items-center vh-100 vw-100">
+    <h1 className="bg-green-200 text-center rounded-xl">Hi Tailwind</h1>
+    <Embed channel="shreya" someobj={obj}/>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwindcss/src/Embed1.jsx
@@ -0,0 +1,13 @@
+function Embed({channel}){       //we have directly used the variable instead of prop.channel
+   return(<>
+ <div class="w-60 h-80 bg-neutral-800 rounded-3xl text-neutral-300 p-4 flex flex-col items-start justify-center gap-3 hover:bg-gray-900 hover:shadow-2xl hover:shadow-sky-400 transition-shadow">
+  <div class="w-52 h-40 bg-sky-300 rounded-2xl"></div>
+  <div class="">
+      <p class="font-extrabold">{channel}</p>
+      <p class="">4 popular types of cards in UI design.</p>
+  </div>
+  <button class="bg-sky-700 font-extrabold p-2 px-6 rounded-xl hover:bg-sky-500 transition-colors">See more</button>
+</div>
+</>)
+}
+export default Embed
