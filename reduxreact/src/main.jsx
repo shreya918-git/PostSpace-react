10394a54e16b6bab332144ca80fc915188e480cb
shreya918-git <shreyasharma918nds@gmail.com>
2024-11-08 02:05:35 +0530

    namechange

--- reduxreact/src/main.jsx
+++ reduxreact/src/main.jsx
@@ -3,8 +3,8 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import {Storeconfigure} from './app/redux.js'
-createRoot(document.getElementById('root')).render(
-  <Provider store={Storeconfigure}>
-    <App />
-  </Provider>
-)
+  createRoot(document.getElementById('root')).render(
+    <Provider store={Storeconfigure}>
+      <App />
+    </Provider>,
+  );
--- router-react/src/components/footer/footer.jsx
+++ router-react/src/components/footer/footer.jsx
@@ -73,8 +73,8 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                        <a className="hover:underline">
+                            shreyasharma
                         </a>
                         . All Rights Reserved.
                     </span>
